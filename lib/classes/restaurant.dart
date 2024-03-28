c5b717a46667e561b63b9828ba0e6edd00cc4fb4
EetuHolmi <eetu.holmi@gmail.com>
2024-03-28 16:02:05 +0200

    implementation of restaurant_page works with test values

--- lib/classes/restaurant.dart
+++ lib/classes/restaurant.dart
@@ -29,5 +29,47 @@ class Restaurant{
       );
     return testRestaurant;
   }
+
+  static List<Restaurant> getTestRestaurantList(){
+    List<Restaurant> restaurantList = [];
+
+    restaurantList.add(Restaurant(
+      name: 'Test Restaurant',
+      foodChain: 'Tests_FoodChain',
+      address: 'Testitie 1',
+      menu: FoodItem.getTestMenu()
+      ));
+
+    restaurantList.add(Restaurant(
+      name: 'Test Restaurant 2',
+      foodChain: 'Tests_FoodChain2',
+      address: 'Testitie 2',
+      menu: FoodItem.getTestMenu()
+      ));
+
+    restaurantList.add(Restaurant(
+      name: 'Test Restaurant 3',
+      foodChain: 'Tests_FoodChain3',
+      address: 'Testitie 3',
+      menu: FoodItem.getTestMenu()
+      ));
+
+    restaurantList.add(Restaurant(
+      name: 'Test Restaurant 4',
+      foodChain: 'Tests_FoodChain4',
+      address: 'Testitie 4',
+      menu: FoodItem.getTestMenu()
+      ));
+
+    restaurantList.add(Restaurant(
+      name: 'Test Restaurant 5',
+      foodChain: 'Tests_FoodChain5',
+      address: 'Testitie 5',
+      menu: FoodItem.getTestMenu()
+      ));
+
+
+    return restaurantList;
+  }
   
 }
--- lib/restaurant_page.dart
+++ lib/restaurant_page.dart
@@ -1,62 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:kandi/classes/restaurant.dart';
 
-class RestaurantPage extends StatelessWidget {
-  const RestaurantPage({super.key});
+class RestaurantPage extends StatefulWidget {
+   const RestaurantPage({super.key});
+
+    @override
+  State<RestaurantPage> createState() => _RestaurantPageState();
+}
+
+class _RestaurantPageState extends State<RestaurantPage> {
+    List<Restaurant> restaurants = [];
+
+   void _getInitialInfo(){
+    restaurants = Restaurant.getTestRestaurantList();
+   }
 
   @override
   Widget build(BuildContext context) {
+    _getInitialInfo();
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Restaurant Details'),
+        title: const Text('Restaurants'),
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Restaurant Name: ABC Restaurant',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            Text(
-              'Location: XYZ Street, City',
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Meal Details:',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            ListTile(
-              title: Text('Chicken Salad'),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Calories: 300'),
-                  Text('Fat: 10g'),
-                  Text('Protein: 20g'),
-                  Text('Carbs: 15g'),
-                ],
-              ),
-            ),
-            ListTile(
-              title: Text('Vegetable Soup'),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Calories: 150'),
-                  Text('Fat: 5g'),
-                  Text('Protein: 8g'),
-                  Text('Carbs: 20g'),
-                ],
-              ),
-            ),
-            // Add more ListTile widgets for other meals
-          ],
-        ),
-      ),
+      body: _restaurantView(),
     );
   }
+
+  ListView _restaurantView() {
+    return ListView.builder(
+      itemCount: restaurants.length,
+      itemBuilder: (context, index) {
+        return Container(
+          margin: const EdgeInsets.all(8),
+          height: 160,
+          color: Colors.green,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '${restaurants[index].name}  |  ${restaurants[index].address}'
+                  ),
+                  SizedBox(
+                  height: 120,
+                  child: _restaurantMenu(index),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  ListView _restaurantMenu(int index) {
+    return ListView.builder(
+      itemCount: restaurants[index].menu.length,
+      scrollDirection: Axis.horizontal,
+      itemBuilder: (menuContext, menuIndex) {
+        return Container(
+          margin: const EdgeInsets.all(8),
+          height: 100,
+          width: 200,
+          color: Colors.red,
+          child: Column(
+            children: [
+              Text(
+                restaurants[index].menu[menuIndex].name
+              ),
+              Text(
+                '${restaurants[index].menu[menuIndex].totalCalories} kcal'
+              )
+            ],
+          ),
+        );
+      }
+    );
+  }
+
 }
